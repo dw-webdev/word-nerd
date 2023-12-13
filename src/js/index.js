20874ca3676504e2879c56959e1770f027253a7c
David Wells <david@dw-webdev.com>
2023-12-13 12:24:50 -0500

    Installed Parcel and Phaser, added Menu and Play scenes

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import Phaser from 'phaser';
+
+import Menu from './scenes/Menu';
+import Play from './scenes/Play';
+
+const game = new Phaser.Game({
+    width: 540,
+    height: 960,
+    type: Phaser.AUTO,
+    parent: 'phaser-game'
+});
+
+game.scene.add('menu', Menu);
+game.scene.add('play', Play);
+
+game.scene.start('menu');
---
+++ src/js/scenes/Menu.js
@@ -0,0 +1,13 @@
+import Phaser from 'phaser';
+
+export default class Menu extends Phaser.Scene {
+
+    preload() {
+
+    }
+
+    create() {
+
+        this.add.text(400, 250, "Word Nerd");
+    }
+}
