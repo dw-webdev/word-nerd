d89b148196106c555a3f4196e2b6715b85c99047
David Wells <david@dw-webdev.com>
2023-12-18 10:20:56 -0500

    Tiles can be selected and will change based on internal timers

--- src/js/objects/LetterTile.js
+++ src/js/objects/LetterTile.js
@@ -2,39 +2,83 @@ import Phaser from 'phaser';
 
 const LETTER_FREQUENCY = "eeeeeeeeeeeeetttttttttaaaaaaaaooooooooiiiiiiinnnnnnnsssssshhhhhhrrrrrrddddllllcccuuummwwffggyyppbbvkjxqz";
 
+const TimerState = {
+    NONE: 0,
+    COUNT_UP: 1,
+    COUNT_DOWN: 2
+};
+
 export default class LetterTile extends Phaser.GameObjects.Container {
 
-    constructor(scene, x, y) {
+    constructor(scene, x, y, index) {
 
         super(scene, x, y);
 
         this.tile = this.scene.add.sprite(0, 0, 'letter-tile');
         this.tile.setOrigin(0, 0);
+        this.tile.setInteractive();
+        this.tile.setData('index', index);
+        this.tile.setData('selected', false);
+        this.tile.on('pointerdown', this.onClick);
         this.add(this.tile);
 
-        this.timer = this.scene.add.graphics();
-        this.timer.x = 42.5;
-        this.timer.y = 42.5;
-        this.timer.angle = -90;
-        this.add(this.timer);
+        this.timerGraphic = this.scene.add.graphics();
+        this.timerGraphic.x = 42.5;
+        this.timerGraphic.y = 42.5;
+        this.timerGraphic.angle = -90;
+        this.add(this.timerGraphic);
         
         this.letter = this.scene.add.text(42.5, 5, "", { font: ' 85px "Courier New", monospace' });
         this.letter.setOrigin(0.5, 0);
         this.add(this.letter);
 
+        this.timerCurrent = 0;
+        this.timerLimit = 0;
+        this.timerState = TimerState.NONE;
+
         this.refresh();
     }
 
+    update(_, delta) {
+
+        if(this.timerState === TimerState.COUNT_UP) {
+            this.tile.setData('selected', false);
+            this.timerCurrent += delta;
+            if(this.timerCurrent >= this.timerLimit) {
+                this.letter.text = this.pickLetter();
+                this.timerCurrent = 0;
+                this.timerLimit = 0;
+                this.timerState = TimerState.NONE;
+            }
+        }
+        if(this.timerState === TimerState.COUNT_DOWN) {
+            this.timerCurrent -= delta;
+            if(this.timerCurrent <= 0) {
+                this.letter.text = "";
+                this.timerCurrent = 0;
+                this.timerLimit = 15000;
+                this.timerState = TimerState.COUNT_UP;
+            }
+        }
+        if(this.timerState === TimerState.NONE && Math.random() < 0.00005) {
+            this.timerCurrent = 15000;
+            this.timerLimit = 15000;
+            this.timerState = TimerState.COUNT_DOWN;
+        }
+
+        const selected = this.tile.getData('selected');
+        this.tile.setFrame(this.timerState === TimerState.COUNT_UP ? 4 : this.timerState === TimerState.COUNT_DOWN ? selected ? 3 : 2 : selected ? 1 : 0);
+
+        this.timerGraphic.clear();
+        if(this.timerCurrent > 0) {
+            this.timerGraphic.lineStyle(10, this.timerState === TimerState.COUNT_DOWN ? 0xfff799 : 0xcccccc, 1);
+            this.timerGraphic.beginPath();
+            this.timerGraphic.arc(0, 0, 27.5, 0, Math.PI * 2 * (this.timerCurrent / this.timerLimit));
+            this.timerGraphic.strokePath();
+        }
+    }
+
     refresh() {
-
-        this.tile.setFrame(Math.floor(Math.random() * 2));
-
-        this.timer.clear();
-        this.timer.fillStyle(0xffffff, 0.2);
-        this.timer.beginPath();
-        this.timer.arc(0, 0, 32.5, 0, Math.PI * 2 * Math.random());
-        this.timer.lineTo(0, 0);
-        this.timer.fillPath();
 
         this.letter.text = this.pickLetter();
     }
@@ -42,5 +86,11 @@ export default class LetterTile extends Phaser.GameObjects.Container {
     pickLetter() {
 
         return LETTER_FREQUENCY.charAt(Math.floor(LETTER_FREQUENCY.length * Math.random())).toUpperCase();
+    }
+
+    onClick(_) {
+
+        console.log(this.getData('index'));
+        this.setData('selected', !this.getData('selected'));
     }
 }
--- src/js/scenes/Play.js
+++ src/js/scenes/Play.js
@@ -9,18 +9,25 @@ const GRID_HEIGHT = 6;
 const GRID_SPACE_X = 85;
 const GRID_SPACE_Y = 85;
 
-export default class Menu extends Phaser.Scene {
+export default class Play extends Phaser.Scene {
 
     preload() {
 
-        this.load.spritesheet('letter-tile', imgLetterTile, { frameWidth: 85, frameHeight: 85, endFrame: 2 });
+        this.load.spritesheet('letter-tile', imgLetterTile, { frameWidth: 85, frameHeight: 85, endFrame: 4 });
     }
 
     create() {
 
+        this.tiles = [];
         for(let i = 0, l = GRID_WIDTH * GRID_HEIGHT; i < l; i++) {
 
-            this.add.existing(new LetterTile(this, (i % GRID_WIDTH) * GRID_SPACE_X + GRID_X, Math.floor(i / GRID_HEIGHT) * GRID_SPACE_Y + GRID_Y));
+            this.tiles[i] = new LetterTile(this, (i % GRID_WIDTH) * GRID_SPACE_X + GRID_X, Math.floor(i / GRID_HEIGHT) * GRID_SPACE_Y + GRID_Y, i);
+            this.add.existing(this.tiles[i]);
         }
+    }
+
+    update(time, delta) {
+
+        this.tiles.forEach(tile => tile.update(time, delta));
     }
 }
