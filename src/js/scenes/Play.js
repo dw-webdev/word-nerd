c702b4d6db28364ec8d13c7d2d139d715c92d4d8
David Wells <david@dw-webdev.com>
2023-12-20 12:15:07 -0500

    Added dictionary, words are validated against dictionary

--- src/js/scenes/Play.js
+++ src/js/scenes/Play.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import LetterTile from '../objects/LetterTile.js';
 import imgLetterTile from '../../img/letter-tile.png';
+import dictionary from '../../data/dictionary.json';
 
 const GRID_X = 15;
 const GRID_Y = 165;
@@ -9,11 +10,14 @@ const GRID_HEIGHT = 6;
 const GRID_SPACE_X = 85;
 const GRID_SPACE_Y = 85;
 
+const MIN_WORD_LENGTH = 5;
+
 export default class Play extends Phaser.Scene {
 
     preload() {
 
         this.load.spritesheet('letter-tile', imgLetterTile, { frameWidth: 85, frameHeight: 85, endFrame: 4 });
+        this.load.json('dictionary', dictionary);
     }
 
     create() {
@@ -25,6 +29,9 @@ export default class Play extends Phaser.Scene {
         this.buttonClear.setOrigin(1, 0);
         this.buttonClear.setInteractive();
         this.buttonClear.on('pointerdown', this.clearWord.bind(this));
+
+        this.result = this.add.text(270, 100, "Must be at least 5 letters", { font: ' 32px "Courier New", monospace' });
+        this.result.setOrigin(0.5, 0);
 
         this.tiles = [];
         for(let i = 0, l = GRID_WIDTH * GRID_HEIGHT; i < l; i++) {
@@ -41,15 +48,35 @@ export default class Play extends Phaser.Scene {
 
     clickTile(letter) {
 
-        console.log(letter);
-
         this.word.text += letter;
+
+        if(this.word.text.length < MIN_WORD_LENGTH) {
+            this.result.text = "Must be at least 5 letters";
+        }
+        else {
+            const valid = this.checkWord(this.word.text);
+            this.result.text = valid ? "Valid" : "Invalid";
+        }
+    }
+
+    checkWord(word) {
+
+        if(word.length < MIN_WORD_LENGTH) return false;
+
+        let dict = this.cache.json.get('dictionary');
+
+       if(!dict[word.charAt(0)]) return false;
+       if(!dict[word.charAt(0)][word.charAt(1)]) return false;
+       if(!dict[word.charAt(0)][word.charAt(1)][word.charAt(2)]) return false;
+
+       return dict[word.charAt(0)][word.charAt(1)][word.charAt(2)].includes(word);
     }
 
     clearWord() {
 
         this.tiles.forEach(tile => tile.deselect());
         this.word.text = "";
+        this.result.text = "Must be at least 5 letters";
     }
 
     onTimeout() {
